4959959b2f0726122760fa6fe7e55a78c565c438
Maximous Black <maximousblk@gmail.com>
2021-02-07 10:59:12 +0000

    chore: fix dependencies' versions

--- src/deps.ts
+++ src/deps.ts
@@ -3,8 +3,8 @@ export { parse as parseFlags } from "https://deno.land/std@0.86.0/flags/mod.ts";
 export { format as formatTime } from "https://deno.land/std@0.86.0/datetime/mod.ts";
 
 // commit
-export { parse as parseCommit } from "https://deno.land/x/commit/mod.ts";
-export type { Commit } from "https://deno.land/x/commit/mod.ts";
+export { parse as parseCommit } from "https://deno.land/x/commit@0.1.5/mod.ts";
+export type { Commit } from "https://deno.land/x/commit@0.1.5/mod.ts";
 
 // octokit
-export { Octokit } from "https://cdn.skypack.dev/@octokit/core";
+export { Octokit } from "https://cdn.skypack.dev/@octokit/core@^3.2.0";
