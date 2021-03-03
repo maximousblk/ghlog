f0c5cb3cc13c4661269bc34c7d25e18f7f129098
Maximous Black <maximousblk@gmail.com>
2021-03-03 06:25:19 +0000

    fix: handle 'no commits' in default template

--- mod.ts
+++ mod.ts
@@ -27,35 +27,35 @@ export async function getDefaultChangelog(
 
   const title = release?.name ? `# ${release?.name}\n\n` : "";
 
-  const counts = changes
-    .map(({ emoji, count }) => `\`${emoji} ${count}\``)
-    .join(" ");
+  const counts = changes.map(({ emoji, count }) => `\`${emoji} ${count}\``);
 
   const stats = [
     `\`📆 ${release?.date ?? formatTime(new Date(), "dd.MM.yyyy")}\``,
     `\`🏷️ ${release?.tag ?? "UNRELEASED"}\``,
     `\`💾 ${_meta.commits.head.shortSha.toUpperCase()}\``,
-    counts,
+    `${counts.join(" ")}`,
     `\`👥 ${_meta.contributors.length}\``,
   ].join(" ");
 
   return `${title}${stats}
 ${
-    changes
-      .map(({ emoji, title, commits }) => {
-        const header = `## ${emoji} ${title}`;
-        const changes = commits
-          .map(({ shortSha, url, header, author }) => {
-            return `- [\`${shortSha.toUpperCase()}\`](${url}) ${header} (${author})`;
-          })
-          .join("\n");
-        return `\n${header}\n\n${changes}`;
-      })
-      .join("\n")
+    changes.length
+      ? changes
+        .map(({ emoji, title, commits }) => {
+          const header = `## ${emoji} ${title}`;
+          const changes = commits
+            .map(({ shortSha, url, header, author }) => {
+              return `- [\`${shortSha.toUpperCase()}\`](${url}) ${header} (${author})`;
+            })
+            .join("\n");
+          return `\n${header}\n\n${changes}`;
+        })
+        .join("\n")
+      : "\nNo changes"
   }
 
 ## 👥 Contributors
 
-${_meta.contributors.map((name) => `- ${name}`).join("\n")}
+${_meta.contributors.map((name) => `- ${name}`).join("\n") || "No contributors"}
 `;
 }
