9cdd4c34a3bc65cddd831fbfaa32fc10ec0752f6
Maximous Black <maximousblk@gmail.com>
2021-08-26 21:06:48 +0000

    chore(deps): upgrade dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,9 +1,9 @@
 // std
-export { parse as parseFlags } from "https://deno.land/std@0.87.0/flags/mod.ts";
-export { format as formatTime } from "https://deno.land/std@0.87.0/datetime/mod.ts";
+export { parse as parseFlags } from "https://deno.land/std@0.106.0/flags/mod.ts";
+export { format as formatTime } from "https://deno.land/std@0.106.0/datetime/mod.ts";
 
 // commit
 export { parse as parseCommit } from "https://deno.land/x/commit@0.1.5/mod.ts";
 
 // octokit
-export { Octokit } from "https://cdn.skypack.dev/@octokit/core@^3.2.0";
+export { Octokit } from "https://cdn.skypack.dev/@octokit/core@^3.5.1";
